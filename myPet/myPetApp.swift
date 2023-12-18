a51d18952e19ba8e115a905e1e4c68e3b6ee7000
deffsin <deffsin@gmail.com>
2023-12-18 13:54:04 +0200

    Initial Commit

---
+++ myPet/myPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPetApp.swift
+//  myPet
+//
+//  Created by Denis Sinitsa on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct myPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
