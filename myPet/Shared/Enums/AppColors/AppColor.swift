3185ccf7d6be6ae2611f160295c75cbe3a4bb4ba
deffsin <deffsin@gmail.com>
2023-12-24 16:03:32 +0200

    Implemented new cases to the AppColor enum and changed DarkBackground

--- myPet/Shared/Enums/AppColors/AppColor.swift
+++ myPet/Shared/Enums/AppColors/AppColor.swift
@@ -10,6 +10,8 @@ import SwiftUI
 enum AppColor {
     case greenColor
     case whiteColor
+    case lightColorBackground
+    case darkColorBackground
     
     var associatedColor: UIColor {
         switch self {
@@ -17,6 +19,10 @@ enum AppColor {
             return UIColor(red: 0.10, green: 0.75, blue: 0.40, alpha: 1.0)
         case .whiteColor:
             return UIColor(red: 0.92, green: 0.92, blue: 0.92, alpha: 1.0)
+        case .lightColorBackground:
+            return UIColor(red: 0.38, green: 0.38, blue: 0.41, alpha: 1.0)
+        case .darkColorBackground:
+            return UIColor(red: 0.1, green: 0.1, blue: 0.12, alpha: 1.0)
         }
     }
 }
