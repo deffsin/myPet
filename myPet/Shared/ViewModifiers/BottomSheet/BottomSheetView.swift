6f1efceaed3920c33550a7d551efa86023cc6d0a
deffsin <deffsin@gmail.com>
2023-12-24 16:01:43 +0200

    Added custom BottomSheet and RoundedCorners

---
+++ myPet/Shared/ViewModifiers/BottomSheet/BottomSheetView.swift
@@ -0,0 +1,62 @@
+//
+//  BottomSheetView.swift
+//  myPet
+//
+//  Created by Denis Sinitsa on 24.12.2023.
+//
+
+import SwiftUI
+
+struct BottomSheetView: View {
+    @Binding var isShowing: Bool
+    var height: CGFloat = 100
+
+    var body: some View {
+        
+        ZStack(alignment: .bottom) {
+            if isShowing {
+                Color.black
+                    .opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            isShowing = false
+                        }
+                    }
+                
+                VStack {
+                    HStack {
+                        Text("Sheet!")
+                        Spacer()
+                        Button(action: {
+                            isShowing = false
+                        }) {
+                            Image(systemName: "xmark")
+                                .font(.system(size: 22))
+                                .foregroundColor(.white)
+                                .padding(5)
+                                .background(Color(appColor: .greenColor))
+                                .clipShape(Circle())
+                        }
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: height)
+                .padding([.horizontal, .vertical], 15)
+                .background(LinearGradient(colors: [Color(appColor: .darkColorBackground), Color(appColor: .lightColorBackground)], startPoint: .top, endPoint: .bottom).opacity(0.6))
+                .cornerRadius(16, corners: [.topLeft, .topRight])
+                .transition(.opacity.combined(with: .move(edge: .bottom)))
+            }
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+        .animation(.easeInOut, value: isShowing)
+        
+    }
+}
+
+#Preview {
+    BottomSheetView(isShowing: .constant(false))
+}
